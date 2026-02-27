ad740f27d8755b869d132292e34375f01a732b5f
Ppunpprem <151167611+Ppunpprem@users.noreply.github.com>
2026-02-27 22:29:14 +0700

    deploy sucessfull

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
-export const API_BASE_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
-  ? "http://127.0.0.1:5000"
-  : "https://tocflix.onrender.com";
+export const API_BASE_URL =
+  window.location.hostname === "localhost" ||
+  window.location.hostname === "127.0.0.1"
+    ? "http://127.0.0.1:5000"
+    : "https://movie-backend-450562655172.asia-southeast1.run.app";
